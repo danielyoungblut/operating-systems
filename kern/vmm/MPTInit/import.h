d007651ae4a1f40a177400c6c52e7cbae1a07853
Mengqi Liu <mengqi.liu@yale.edu>
2017-09-20 00:07:17 -0400

    lab2 cont'd

---
+++ kern/vmm/MPTInit/import.h
@@ -0,0 +1,12 @@
+#ifndef _INIT_MM_MPTINIT_H_
+#define _INIT_MM_MPTINIT_H_
+
+#ifdef _KERN_
+
+void pdir_init_kern(unsigned int);
+void set_pdir_base(unsigned int);
+void enable_paging(void);
+
+#endif /* _KERN_ */
+
+#endif /* !_INIT_MM_MPTINIT_H_ */
